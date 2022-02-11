69d7ce3428ec4f62409b0f6c772923d24f1a3ca0
Adam KEDDIS <adam.keddis@ens.uvsq.fr>
2022-02-11 12:10:53 +0100

    Fenetre

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -7,13 +7,13 @@
 #https://github.com/uvsq22103405/projet_tas_de_sable.git
 #########################################
 # import des librairies
-
+from tkinter import *
 
 
 #########################################
 # définition des constantes (écrites en majuscule)
-
-
+HAUTEUR = 600
+LARGEUR = 600 
 
 #########################################
 # définition des variables globales
@@ -22,7 +22,18 @@
 #########################################
 # définition des fonctions (chaque fonction devra contenir une docstring)
 
+#########################################
+# partie principale 
 
+
+
+
+
+# Création des widgets 
+FENETRE = Tk()
+FENETRE.title("Tas de sable")
+canvas = Canvas(FENETRE, height=HAUTEUR, width=LARGEUR)
+canvas.grid(column=1, row=0)
 #########################################
 #programme principal contenant la définition des widgets et des événements qui leur sont liés et
 #l’appel à la boucle de gestion des événements
@@ -30,4 +41,5 @@
 
 #########################################
 
+FENETRE.mainloop()
 

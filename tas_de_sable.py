0e92dc2ef12a6a481c5160719eaa169628b046f0
Laura LEFEVRE <laura.lefevre@ens.uvsq.fr>
2022-02-11 11:58:50 +0100

    structure programme

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -1,9 +1,33 @@
 #########################################
+# informations liées au groupe
 #groupe LDDBI
 # LAURA LEFEVRE
 # ADAM KEDDIS
 # MANOUR INES 
 #https://github.com/uvsq22103405/projet_tas_de_sable.git
-#############################################
+#########################################
+# import des librairies
+
+
+
+#########################################
+# définition des constantes (écrites en majuscule)
+
+
+
+#########################################
+# définition des variables globales
+
+
+#########################################
+# définition des fonctions (chaque fonction devra contenir une docstring)
+
+
+#########################################
+#programme principal contenant la définition des widgets et des événements qui leur sont liés et
+#l’appel à la boucle de gestion des événements
+
+
+#########################################
 
 

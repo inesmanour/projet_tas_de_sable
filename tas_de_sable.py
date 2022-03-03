27e1f9deb0da6b3488453b7a45a5255feab67cb2
Adam KEDDIS <adam.keddis@ens.uvsq.fr>
2022-03-03 17:59:20 +0100

    test

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -14,6 +14,8 @@ from glob import glob
 from tkinter import *
 from random import *
 from functools import partial
+from winreg import REG_OPENED_EXISTING_KEY
+
 
 #########################################
 # constantes
@@ -81,9 +83,43 @@ def random_config(n):
     global config_courante
     for ligne in range(n):
         for colonne in range(n):
-            config_courante[ligne][colonne] += randint(0,3)
+            config_courante[ligne][colonne] = randint(0,3)  # il faut écraser la variable et pas l'additionner !!!
     maj_grille(n)
 
+
+
+
+
+" creation d'une configuration par clic de l'utilisateur "
+
+def clic(event):
+    ''' fonction permettant d'ajouter 1 grain de sable sur la case cliquée avec un clic gauche'''
+    global x, y 
+    x, y = event.x, event.y
+    x=int(x/12)
+    y= int(y /12)
+   
+    print(x,y)
+    config_courante[y][x] += 1 
+
+    maj_grille(n)
+
+def clic2(event): 
+    ''' fonction permettant de soustraire 1 grain de sable sur la case cliquée avec un clic droit'''
+    global x, y 
+    x, y = event.x, event.y
+    x=int(x/12)
+    y= int(y /12)
+    print("cc")
+
+    if config_courante[y][x] > 0 :
+        config_courante[y][x] -= 1 
+    else :
+       pass
+
+    maj_grille(n)
+def essaye(event):
+    print(event.x)
 #########################################
 # partie principale 
 
@@ -93,11 +129,17 @@ fenetre = Tk()
 fenetre.title("Projet 1 : Tas de sable")
 canevas = Canvas(fenetre, height=HAUTEUR, width=LARGEUR, bg = "snow")
 boutton_random_config = Button(fenetre, text = "Configuration aléatoire", width=20, height=10, bg="moccasin", fg="darksalmon", command = partial(random_config, n))
+boutton_addition = Button(fenetre, text= "Addition", width=20, height=10, bg="moccasin", fg="darksalmon")
+canevas.bind("<Button-1>", clic) 
+canevas.bind("<Button-2>", essaye)
 
 # placement des widgets
 canevas.grid(column=1, row=0, columnspan = 2, padx = 3, pady = 3)
 boutton_random_config.grid(column=0, row=0)
+boutton_addition.grid(column=0, row=1)
 
+
+ 
 # boucle principale
 tas_de_sable(n)
-fenetre.mainloop()
+fenetre.mainloop() 
